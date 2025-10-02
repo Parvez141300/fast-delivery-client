d1cab1d956739de5e01175a3ded6b51891437dbc
Parvez141300 <parvezhossain744471@gmail.com>
2025-10-02 15:29:12 +0600

    created navbar, home page and also added the theme for light and ddark mode in index.css file

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import NavBar from '../pages/shared/NavBar.jsx/NavBar';
 
 const RootLayout = () => {
     return (
         <div>
             {/* navbar */}
-            <nav>
-
+            <nav className='bg-base-100 shadow-sm'>
+                <NavBar></NavBar>
             </nav>
             {/* for routing purpose */}
-            <main>
+            <main className='w-10/12 mx-auto my-8'>
                 <Outlet></Outlet>
             </main>
             {/* footer */}
