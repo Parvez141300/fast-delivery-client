340fc7c6bee363670c02b3a9dfca7d425896a02c
Parvez141300 <parvezhossain744471@gmail.com>
2025-10-03 02:53:15 +0600

    edited the banner

--- src/pages/Home/shared/Banner/Banner.jsx
+++ src/pages/Home/shared/Banner/Banner.jsx
@@ -17,6 +17,8 @@ import image1 from "../../../../assets/banner-images/image1.jpg";
 import image2 from "../../../../assets/banner-images/image2.jpg";
 import image3 from "../../../../assets/banner-images/image3.jpg";
 
+const bannerImages = [image1, image2, image3];
+
 const Banner = () => {
   const progressCircle = useRef(null);
   const progressContent = useRef(null);
@@ -41,35 +43,17 @@ const Banner = () => {
         onAutoplayTimeLeft={onAutoplayTimeLeft}
         className="min-h-[30vh] md:min-h-[50vh] rounded-lg"
       >
-        <SwiperSlide>
-          <div className="relative w-full h-[250px] sm:h-[350px] md:h-[450px] lg:h-[600px]">
-            <img
-              src={image1}
-              className="w-full h-full object-cover"
-              alt="image 1"
-            />
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="relative w-full h-[250px] sm:h-[350px] md:h-[450px] lg:h-[600px]">
-            <img
-              src={image2}
-              className="w-full h-full object-cover"
-              alt="image 2"
-            />
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="relative w-full h-[250px] sm:h-[350px] md:h-[450px] lg:h-[600px]">
-            <img
-              src={image3}
-              className="w-full h-full object-cover"
-              alt="image 3"
-            />
-          </div>
-        </SwiperSlide>
+        {bannerImages.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className="relative w-full h-[250px] sm:h-[350px] md:h-[450px] lg:h-[600px]">
+              <img
+                src={image}
+                className="w-full h-full object-cover"
+                alt={`image-${index}`}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
 
         <div className="autoplay-progress" slot="container-end">
           <svg viewBox="0 0 48 48" ref={progressCircle}>
