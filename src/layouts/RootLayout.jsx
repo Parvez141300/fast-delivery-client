06ed9db35b77b2b8935697264ef471e4ccf65bee
Parvez141300 <parvezhossain744471@gmail.com>
2025-10-02 16:33:28 +0600

    created footer section and edited the navbar and index.css file and rootlayout.jsx file

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import NavBar from '../pages/shared/NavBar.jsx/NavBar';
+import React from "react";
+import { Outlet } from "react-router";
+import NavBar from "../pages/shared/NavBar.jsx/NavBar";
+import Footer from "../pages/shared/Footer/Footer";
 
 const RootLayout = () => {
-    return (
-        <div>
-            {/* navbar */}
-            <nav className='bg-base-100 shadow-sm'>
-                <NavBar></NavBar>
-            </nav>
-            {/* for routing purpose */}
-            <main className='w-10/12 mx-auto my-8'>
-                <Outlet></Outlet>
-            </main>
-            {/* footer */}
-            <footer>
-
-            </footer>
-        </div>
-    );
+  return (
+    <div>
+      {/* navbar */}
+      <nav className="bg-base-100 shadow-sm">
+        <NavBar></NavBar>
+      </nav>
+      {/* for routing purpose */}
+      <main className="w-10/12 mx-auto my-8 min-h-[calc(100vh-64px)]">
+        <Outlet></Outlet>
+      </main>
+      {/* footer */}
+      <footer className="bg-base-300">
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
 };
 
 export default RootLayout;
--- src/pages/shared/NavBar.jsx/NavBar.jsx
+++ src/pages/shared/NavBar.jsx/NavBar.jsx
@@ -1,6 +1,61 @@
 import React from "react";
+import { Link, NavLink } from "react-router";
 
 const NavBar = () => {
+  const navItems = (
+    <>
+      <li>
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            isActive ? "nav-link active-link" : "nav-link"
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={"/services"}
+          className={({ isActive }) =>
+            isActive ? "nav-link active-link" : "nav-link"
+          }
+        >
+          Services
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={"/coverage"}
+          className={({ isActive }) =>
+            isActive ? "nav-link active-link" : "nav-link"
+          }
+        >
+          Coverage
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={"/about-us"}
+          className={({ isActive }) =>
+            isActive ? "nav-link active-link" : "nav-link"
+          }
+        >
+          About Us
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={"/contact-us"}
+          className={({ isActive }) =>
+            isActive ? "nav-link active-link" : "nav-link"
+          }
+        >
+          Contact Us
+        </NavLink>
+      </li>
+    </>
+  );
   return (
     <div className="navbar max-w-10/12 mx-auto p-0">
       <div className="navbar-start">
@@ -26,51 +81,51 @@ const NavBar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navItems}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl px-0">FastDelivery</a>
+        <Link to={"/"} className="btn btn-ghost text-xl px-0">
+          FastDelivery
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
       </div>
       <div className="navbar-end gap-2">
+        <label className="flex cursor-pointer gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="5" />
+            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+          </svg>
+          <input
+            type="checkbox"
+            value="dark"
+            className="toggle theme-controller"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+        </label>
         <a className="btn btn-primary btn-outline">Login</a>
         <a className="btn btn-primary">Be a rider</a>
       </div>
