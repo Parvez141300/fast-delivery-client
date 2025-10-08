b2b29599831fe64cb703dad7ad8dc46346c3d765
Parvez141300 <parvezhossain744471@gmail.com>
2025-10-09 01:15:10 +0600

    added a function to the login form to get form data

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -3,20 +3,37 @@ import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { MdLockOutline, MdOutlineEmail } from "react-icons/md";
 import { Link } from "react-router";
 import { FcGoogle } from "react-icons/fc";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
+  // const react hook form
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log("login form data:", data);
+  };
   return (
     <div className="bg-base-100 md:max-w-md lg:max-w-lg shadow-2xl p-5">
       <h1 className="text-center text-xl font-bold">Login Now!</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset className="fieldset">
           {/* email field */}
           <label className="label">User Email</label>
           <span className="input validator w-full focus-within:outline-0">
             <MdOutlineEmail size={20} />
-            <input type="email" placeholder="Email" />
+            <input
+              type="email"
+              placeholder="Email"
+              {...register("email", { required: "Email is required" })}
+            />
           </span>
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
           {/* password field */}
           <label className="label">User Password</label>
           <span className="relative input validator w-full focus-within:outline-0">
@@ -24,9 +41,14 @@ const Login = () => {
             <input
               type={showPassword ? "text" : "password"}
               placeholder="Password"
+              {...register("password", {
+                required: "Password is required",
+                maxLength: 10,
+              })}
             />
             {showPassword ? (
               <button
+                type="button"
                 onClick={() => setShowPassword(false)}
                 className="absolute top-1/2 -translate-y-1/2 right-4 z-10 cursor-pointer"
               >
@@ -34,6 +56,7 @@ const Login = () => {
               </button>
             ) : (
               <button
+                type="button"
                 onClick={() => setShowPassword(true)}
                 className="absolute top-1/2 -translate-y-1/2 right-4 z-10 cursor-pointer"
               >
@@ -41,6 +64,14 @@ const Login = () => {
               </button>
             )}
           </span>
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+          {errors.password?.type === "maxLength" && (
+            <p className="text-red-500">
+              Password must be maximum 10 character long
+            </p>
+          )}
           <div className="flex items-center gap-1">
             Don't Have An Account?
             <Link to={"/register"} className="link link-hover">
